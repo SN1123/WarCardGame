1a48fdc58e51b4c3a6f3926fefd60c7d4437b231
Kyle Waltz <kylewaltz@yahoo.com>
2015-01-21 07:50:12 -0700

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,111 +1,106 @@
-// Complete Class card by extending abstract Class PlayingCard and implementing the Comparable interface
-public class Card extends PlayingCard
+public class Card extends PlayingCard implements Comparable<Card>
 {
   public Card(int suit, int rank)
   {
     super(suit, rank);
   }
-  
   public String toString()
   {
-    if(suit = 1)
+    if (this.getSuit()==1)
     {
-      if (rank = 1)
-      {
-        return "S-A";
-      }
-      if (rank = 10)
-      {
-        return "S-T";
-      }
-      if (rank = 11)
-      {
-        return "S-J";
-      }
-      if (rank = 12)
-      {
-        return "S-Q";
-      }
-      if (rank = 13)
-      {
-        return "S-K";
-      }
-    }
-
-    if(suit = 2)
-    {
-      if (rank = 1)
-      {
-        return "H-A";
-      }
-      if (rank = 10)
+      if(this.getRank()==10)
       {
         return "H-T";
       }
-      if (rank = 11)
+       if (this.getRank()==11)
       {
         return "H-J";
       }
-      if (rank = 12)
+       if(this.getRank()==12)
       {
         return "H-Q";
       }
-      if (rank = 13)
+       if(this.getRank()==13)
       {
         return "H-K";
       }
-    }
-
-    if(suit = 3)
-    {
-      if (rank = 1)
+      
       {
-        return "C-A";
+        return "H-";  
       }
-      if (rank = 10)
+    }
+    if (this.getSuit()==2)
+    {
+      if(this.getRank()==10)
       {
         return "C-T";
       }
-      if (rank = 11)
+       if (this.getRank()==11)
       {
         return "C-J";
       }
-      if (rank = 12)
+       if(this.getRank()==12)
       {
         return "C-Q";
       }
-      if (rank = 13)
+       if(this.getRank()==13)
       {
         return "C-K";
       }
-    }
-
-    if(suit = 4)
-    {
-      if (rank = 1)
+      
       {
-        return "D-A";
+        return "C-";
       }
-      if (rank = 10)
+    }
+    if (this.getSuit()==3)
+    {
+      if(this.getRank()==10)
+      {
+        return "S-T";
+      }
+       if (this.getRank()==11)
+      {
+        return "S-J";
+      }
+       if(this.getRank()==12)
+      {
+        return "S-Q";
+      }
+      if(this.getRank()==13)
+      {
+        return "S-K";
+      }
+      
+      {
+        return "S-" ; 
+      }
+    }
+    
+    {
+      if(this.getRank()==10)
       {
         return "D-T";
       }
-      if (rank = 11)
+       if (this.getRank()==11)
       {
         return "D-J";
       }
-      if (rank = 12)
+       if(this.getRank()==12)
       {
         return "D-Q";
       }
-      if (rank = 13)
+       if(this.getRank()==13)
       {
         return "D-K";
       }
-    }    
+      
+      {
+        return "D-";
+      }
+    }
   }
-  public int compareTo()
+  public int compareTo(Card x)
   {
-    return this.getRank()- super.getRank();
+    return this.getRank()-x.getRank();
   }
 }
